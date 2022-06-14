aaf933ddd1af8c3d74db8398ed609c2d01a56783
Bedriye Nur GENÇ <gencbedriyenur@gmail.com>
2022-06-14 18:14:46 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+// hooks
+import { useAppSelector, useAppDispatch } from "./hooks";
+import { useGetBitcoinDataQuery } from "./services/app";
+// actions
+import { changeCurrency } from "./features/appSlice";
+// Styles
+import { Wrapper } from "./App.styles";
+
+// const getBCData = async (): Promise<BitcoinData> =>
+//   await (await fetch("https://blockchain.info/ticker")).json();
+
+// const INTERVAL_TIME = 5000; // ms
+
+const App = () => {
+  const { currency } = useAppSelector((state) => state.app);
+  const dispatch = useAppDispatch();
+  const { data, isLoading, error } = useGetBitcoinDataQuery(undefined);
+
+  const handleCurrencySelection = (e: any) =>
+    dispatch(changeCurrency(e.currentTarget.value));
+
+  if (isLoading) return <div>Loading ...</div>;
+  if (error) return <div>Something went horrible wrong ...</div>;
+
+  return (
+    <Wrapper>
+      <>
+        <h2>Bitcoin Price</h2>
+        <select value={currency} onChange={handleCurrencySelection}>
+          {data &&
+            Object.keys(data).map((currency) => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+        </select>
+        <div>
+          <h2>
+            {data && data[currency].symbol}
+            {data && data[currency].last}
+          </h2>
+        </div>
+      </>
+    </Wrapper>
+  );
+};
+
+export default App;
